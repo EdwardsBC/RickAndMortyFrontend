de65d8c419fbb59626ab375b8ca4bc26a8e3b737
EdwardsBC <M4pX_bc@hotmail.com>
2024-07-09 12:31:30 -0500

    finish

--- src/app/character-list/character-list.component.ts
+++ src/app/character-list/character-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { CharacterService } from '../services/character.service';
 import { Character } from '../models/character.model';
 import { FormsModule } from '@angular/forms';
@@ -16,9 +16,9 @@ export class CharacterListComponent implements OnInit {
   characters: Character[] = [];
   filteredCharacters: Character[] = [];
   searchTerm: string = '';
-  error: string = ''; 
+  error: string = '';
 
-  constructor(private characterService: CharacterService) { }
+  constructor(private characterService: CharacterService, private router: Router) { }
 
   ngOnInit(): void {
     this.characterService.getCharacters().subscribe((data: Character[]) => {
@@ -37,5 +37,9 @@ export class CharacterListComponent implements OnInit {
         character.name.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
     }
+  }
+
+  goToDetails(characterId: number): void {
+    this.router.navigate(['/characters', characterId]);
   }
 }
